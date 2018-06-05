c0bbc2143546127701955fb0f113effa6fc9d2a2
Nisskhen Xenia <nisskhen_kd@ekb.sima-land.com>
2018-06-05 09:27:34 +0500

    SL-15615 Поправила тесты

--- tests/TimeFreezeTest.php
+++ tests/TimeFreezeTest.php
@@ -4,7 +4,9 @@ namespace sima\timecop\tests;
 
 use PHPUnit\Framework\TestCase;
 use sima\timecop\TimeFreeze;
+use yii\di\Instance;
 use yii\web\Application;
+use yii\web\Request;
 use yii\web\Session;
 
 class TimeFreezeTest extends TestCase
@@ -21,27 +23,41 @@ class TimeFreezeTest extends TestCase
             'id' => 'app-timecop',
             'basePath' => dirname(__DIR__)
         ];
+        $today = date('d.m.Y');
         $app = new Application($config);
         $timeFreeze = new TimeFreeze();
         $timeFreeze->session = $this->createMock(Session::class);
         $timeFreeze->session->expects($this->any())
             ->method("setCookieParams")
-            ->will($this->returnValue($this->cookie[$timeFreeze->requestVariable] = $timeFreeze->request->getQueryParam($timeFreeze->requestVariable)));
+            ->willReturn($this->cookie[$timeFreeze->requestVariable] = $timeFreeze->request->getQueryParam($timeFreeze->requestVariable));
         $timeFreeze->session->expects($this->any())
             ->method("getCookieParams")
-            ->will($this->returnValue($this->cookie[$timeFreeze->requestVariable]));
+            ->willReturn($this->cookie[$timeFreeze->requestVariable]);
+        $timeFreeze->session->expects($this->any())
+            ->method("remove")
+            ->willReturnCallback(function () use ($timeFreeze) {
+                $this->cookie[$timeFreeze->requestVariable] = null;
+                timecop_return();
+            });
+
         // 1606780800 = 01.12.2020 12:00am (UTC)
         $timeFreeze->request->setQueryParams([$timeFreeze->requestVariable => 1606780800]);
         $timeFreeze->bootstrap($app);
         $this->assertEquals('01.12.2020',date('d.m.Y'));
+
+        // Сброс даты на текущее время
+        $timeFreeze->request->setQueryParams([$timeFreeze->requestVariable => 'reset']);
+        $timeFreeze->bootstrap($app);
+        $this->assertEquals($today, date('d.m.Y'));
+
         // 804211200 = 27.06.1995 12:00am (UTC)
         $timeFreeze->request->setQueryParams([$timeFreeze->requestVariable => 804211200]);
         $timeFreeze->bootstrap($app);
         $this->assertEquals('27.06.1995',date('d.m.Y'));
+
         // Сброс даты на текущее время
-        $today = date('d.m.Y');
         $timeFreeze->request->setQueryParams([$timeFreeze->requestVariable => 'reset']);
         $timeFreeze->bootstrap($app);
-        $this->assertEquals($today,date('d.m.Y'));
+        $this->assertEquals($today, date('d.m.Y'));
     }
 }
